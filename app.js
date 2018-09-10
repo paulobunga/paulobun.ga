37192516bec3eef290fc0d73e9bbce675e869280
BOLD AFRICA <boldafrica.one@gmail.com>
2018-09-10 22:07:26 +0300

    Added static directory

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.render('index');
 });
